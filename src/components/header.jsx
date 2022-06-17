6aeec8bc350c73eb17d21f729c53578fa06f1f41
milessteamaccount <ronaldlamdev@gmail.com>
2022-06-17 00:25:47 -0500

    added display flex to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Close from '../images/icon-close.svg'
 import Logo from '../images/logo.svg';
 import MobileMenu from '../images/icon-menu.svg';
 import HeaderCart from '../images/icon-cart.svg';
 import Avatar from '../images/image-avatar.png';
 
+// Create List with Link in Header Menu
 const HeaderMenuList = (props) => {
   return (
     <li><a href={props.link}>{props.list}</a></li>
@@ -13,15 +15,16 @@ const HeaderMenuList = (props) => {
 const Header = () => {
   return (
     <header>
+      <img src={Close} alt="close" className="close-icon"/>
       <div className="header-section-1">
         <img src={MobileMenu} alt="mobile menu" className="mobile-menu" />
         <img src={Logo} alt="logo" className="logo" />
-        <ul>
-          <HeaderMenuList />
-          <HeaderMenuList />
-          <HeaderMenuList />
-          <HeaderMenuList />
-          <HeaderMenuList />
+        <ul className="menu-list">
+          <HeaderMenuList link="#collections" list="Collections"/>
+          <HeaderMenuList link="#men" list="Men"/>
+          <HeaderMenuList link="#women" list="Women"/>
+          <HeaderMenuList link="#about" list="About"/>
+          <HeaderMenuList link="#contact" list="Contact"/>
         </ul>
       </div>
       <div className="header-section-2">
