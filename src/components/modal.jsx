9049516f46f57d25b72284b7de731b0308088cf2
milessteamaccount <ronaldlamdev@gmail.com>
2022-07-06 20:42:01 -0500

    added carousel functions to the modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -13,6 +13,7 @@ import ModalThumbnail4 from '../images/image-product-4-thumbnail.jpg';
 
 const Modal = () => {
 
+  // Closes Modal
   const closeModal = () => {
     const modal = document.getElementById('modal');
 
@@ -25,6 +26,27 @@ const Modal = () => {
     }
   }
 
+  // Change image when clicking on the carousel buttons
+  let numModal = 0;
+
+  const prevModalImage = () => {
+    numModal--;
+    let modalSlider = document.getElementById('modal-slider');
+    let modalProductImages = [ModalFullImg1, ModalFullImg2, ModalFullImg3, ModalFullImg4];
+    if (numModal < 0) {
+      numModal = modalProductImages.length - 1;
+    } modalSlider.src = modalProductImages[numModal];
+  }
+
+  const nextModalImage = () => {
+    numModal++;
+    let modalSlider = document.getElementById('modal-slider');
+    let modalProductImages = [ModalFullImg1, ModalFullImg2, ModalFullImg3, ModalFullImg4];
+    if (numModal >= modalProductImages.length) {
+      numModal = 0;
+    } modalSlider.src = modalProductImages[numModal];
+  }
+
   return (
     <section id="modal" className="modal">
       <div className="modal-container">
@@ -33,18 +55,18 @@ const Modal = () => {
         <div className="modal-product-images-container">
           {/* Modal Close Icon */}
           <FontAwesomeIcon onClick={closeModal} className="modal-close" icon={faClose} />
-          <img className="modal-img" src={ModalFullImg1} alt="modal pic" />
-          <img className="modal-img" src={ModalFullImg2} alt="modal pic" hidden/>
+          <img id="modal-slider" className="modal-img" src={ModalFullImg1} alt="modal pic" />
+          {/* <img className="modal-img" src={ModalFullImg2} alt="modal pic" hidden/>
           <img className="modal-img" src={ModalFullImg3} alt="modal pic" hidden/>
-          <img className="modal-img" src={ModalFullImg4} alt="modal pic" hidden/>
+          <img className="modal-img" src={ModalFullImg4} alt="modal pic" hidden/> */}
 
           {/* Previous and Next functions */}
           <div className="modal-carousel-container">
-            <div className="modal-left-chevron modal-chevron-container">
-              <FontAwesomeIcon className="modal-chevron modal-previous" icon={faChevronLeft}/>
+            <div onClick={prevModalImage} className="modal-left-chevron modal-chevron-container">
+              <FontAwesomeIcon onClick={prevModalImage} className="modal-chevron modal-previous" icon={faChevronLeft}/>
             </div>
-            <div className="modal-right-chevron modal-chevron-container">
-              <FontAwesomeIcon className="modal-chevron modal-next" icon={faChevronRight}/>
+            <div onClick={nextModalImage} className="modal-right-chevron modal-chevron-container">
+              <FontAwesomeIcon onClick={nextModalImage} className="modal-chevron modal-next" icon={faChevronRight}/>
             </div>
           </div>
       </div>
